e07e3053226ddeb377652e73482c0e0b924e9628
MakotoOta <e34734@jp.ibm.com>
2016-03-08 19:07:46 +0900

    up

--- pssl_test02.bat
+++ pssl_test02.bat
@@ -3,11 +3,11 @@
 md c:\temp
 
 echo Import-Module BitsTransfer > c:\temp\dl1.ps1
-echo Start-BitsTransfer http://fg.v4.download.windowsupdate.com/msdownload/update/software/updt/2012/10/windowsserver2012-kb2607607-x64-jpn_d079f61ac6b2bab923f14cd47c68c4af0835537f.cab c:\temp\test.cab >> c:\temp\dl1.ps1
+echo Start-BitsTransfer -Source http://fg.v4.download.windowsupdate.com/msdownload/update/software/updt/2012/10/windowsserver2012-kb2607607-x64-jpn_d079f61ac6b2bab923f14cd47c68c4af0835537f.cab -Destination c:\temp\test.cab >> c:\temp\dl1.ps1
 powershell -NoProfile -ExecutionPolicy Unrestricted c:\temp\dl1.ps1
 
 echo Import-Module BitsTransfer > c:\temp\dl2.ps1
-echo Start-BitsTransfer https://raw.githubusercontent.com/MakotoOta/pssl/master/pssl_test03 c:\test.ps1 >> c:\temp\dl2.ps1
+echo Start-BitsTransfer -Source https://raw.githubusercontent.com/MakotoOta/pssl/master/pssl_test03 -Destination c:\test.ps1 >> c:\temp\dl2.ps1
 powershell -NoProfile -ExecutionPolicy Unrestricted c:\temp\dl2.ps1
 
 powershell -NoProfile -ExecutionPolicy Unrestricted c:\test.ps1
