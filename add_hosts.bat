dd516e8f0856dbd00bd8de6b336bd547ef343891
MakotoOta <e34734@jp.ibm.com>
2016-03-11 11:41:11 +0900

    up

---
+++ add_hosts.bat
@@ -0,0 +1,1 @@
+echo 173.193.12.45 otatest16 otatest16.test01.test >> C:\Windows\System32\Drivers\etc\hosts
--- after_remote.ps1
+++ after_remote.ps1
@@ -1,4 +1,10 @@
-$RemoteNode = "xxxx"
+$RemoteNode = "otatest16"
+
+Set-Item WSMan:\localhost\Client\TrustedHosts -Value $RemoteNode -Force
+
+# change Administrator's password and never expire
+Invoke-Command -Computername $RemoteNode -ScriptBlock { net user Administrator password0! }
+Invoke-Command -Computername $RemoteNode -ScriptBlock { wmic useraccount where "Name='Administrator'" set PasswordExpires=FALSE }
 
 Invoke-Command -Computername $RemoteNode -ScriptBlock { c:\after01.bat }
 
