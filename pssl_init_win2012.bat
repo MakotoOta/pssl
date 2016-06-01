fe471beebfe261d7e4846ea34e3c58381c1f6d7d
MakotoOta <e34734@jp.ibm.com>
2016-06-01 14:42:33 +0900

    2016/6/1

--- pssl_init_win2012.bat
+++ pssl_init_win2012.bat
@@ -2,22 +2,22 @@ rem management server registration to hosts file
 rem echo "" >> C:\Windows\System32\Drivers\etc\hosts
 rem echo 10.37.56.218 otatest33 otatest33.test01.test >> C:\Windows\System32\Drivers\etc\hosts
 
-md c:\temp
+rem md c:\temp
 
-echo Invoke-WebRequest -uri http://fg.v4.download.windowsupdate.com/msdownload/update/software/updt/2012/10/windowsserver2012-kb2607607-x64-jpn_d079f61ac6b2bab923f14cd47c68c4af0835537f.cab -outfile c:\after.cab > c:\temp\dl1.ps1
-powershell -NoProfile -ExecutionPolicy Unrestricted c:\temp\dl1.ps1
+rem echo Invoke-WebRequest -uri http://fg.v4.download.windowsupdate.com/msdownload/update/software/updt/2012/10/windowsserver2012-kb2607607-x64-jpn_d079f61ac6b2bab923f14cd47c68c4af0835537f.cab -outfile c:\after.cab > c:\temp\dl1.ps1
+rem powershell -NoProfile -ExecutionPolicy Unrestricted c:\temp\dl1.ps1
 
-echo Invoke-WebRequest -uri https://raw.githubusercontent.com/MakotoOta/pssl/master/pssl_init_win2012_01_ps1 -outfile c:\after01.ps1 > c:\temp\dl2.ps1
-powershell -NoProfile -ExecutionPolicy Unrestricted c:\temp\dl2.ps1
+rem echo Invoke-WebRequest -uri https://raw.githubusercontent.com/MakotoOta/pssl/master/pssl_init_win2012_01_ps1 -outfile c:\after01.ps1 > c:\temp\dl2.ps1
+rem powershell -NoProfile -ExecutionPolicy Unrestricted c:\temp\dl2.ps1
 
-echo Invoke-WebRequest -uri https://raw.githubusercontent.com/MakotoOta/pssl/master/pssl_init_win2012_01_bat -outfile c:\after01.bat > c:\temp\dl3.ps1
-powershell -NoProfile -ExecutionPolicy Unrestricted c:\temp\dl3.ps1
+rem echo Invoke-WebRequest -uri https://raw.githubusercontent.com/MakotoOta/pssl/master/pssl_init_win2012_01_bat -outfile c:\after01.bat > c:\temp\dl3.ps1
+rem powershell -NoProfile -ExecutionPolicy Unrestricted c:\temp\dl3.ps1
 
-echo Invoke-WebRequest -uri https://raw.githubusercontent.com/MakotoOta/pssl/master/pssl_init_win2012_02_ps1 -outfile c:\after02.ps1 > c:\temp\dl4.ps1
-powershell -NoProfile -ExecutionPolicy Unrestricted c:\temp\dl4.ps1
+rem echo Invoke-WebRequest -uri https://raw.githubusercontent.com/MakotoOta/pssl/master/pssl_init_win2012_02_ps1 -outfile c:\after02.ps1 > c:\temp\dl4.ps1
+rem powershell -NoProfile -ExecutionPolicy Unrestricted c:\temp\dl4.ps1
 
-echo Invoke-WebRequest -uri https://raw.githubusercontent.com/MakotoOta/pssl/master/pssl_init_win2012_02_bat -outfile c:\after02.bat > c:\temp\dl5.ps1
-powershell -NoProfile -ExecutionPolicy Unrestricted c:\temp\dl5.ps1
+rem echo Invoke-WebRequest -uri https://raw.githubusercontent.com/MakotoOta/pssl/master/pssl_init_win2012_02_bat -outfile c:\after02.bat > c:\temp\dl5.ps1
+rem powershell -NoProfile -ExecutionPolicy Unrestricted c:\temp\dl5.ps1
 
 rem echo Set-Item WSMan:\localhost\Client\TrustedHosts -Value otatest33 -Force > c:\temp\dl6.ps1
 rem powershell -NoProfile -ExecutionPolicy Unrestricted c:\temp\dl6.ps1
@@ -35,6 +35,6 @@ rem net user testadmin password0! /add /active:yes /expires:never /fullname:"Tes
 rem net localgroup "Administrators" "testadmin" /add
 
 rem change Timezone
-tzutil /s "Tokyo Standard Time"
+rem tzutil /s "Tokyo Standard Time"
 
-rd /s /q c:\temp
+rem rd /s /q c:\temp
